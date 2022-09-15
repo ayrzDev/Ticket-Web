dadfa76841934846f5d4a017eeb4323e7e7cc4da
Ayrz <myfrother@gmail.com>
2022-09-15 15:53:37 +0300

    Navbar and Header renewed

--- apps/includes/navbar.php
+++ apps/includes/navbar.php
@@ -1,11 +1,15 @@
 <div class="d-flex simplenavbar text-center justify-content-center">
-
     <img class="rounded"
         src="https://media-exp1.licdn.com/dms/image/C4E0BAQH-7IWMa2U8yg/company-logo_200_200/0/1636639926141?e=2147483647&v=beta&t=bHVWBfjr9hmEOvHFKZK0AcImZCsddan92MB_l0CYZJc"
         alt="" width="50px">
-</div>
-<div class="d-flex simplenavbar text-center justify-content-center">
-    <a href="./index.php" class="navelement">Ana Sayfa</a>
-    <a href="./mysupport.php" class="navelement">Destek Taleplerim</a>
-    <a href="./addsupport.php" class="navelement btn-orange">Yeni Destek Talebi Aç</a>
+        <div class="simplenavbar text-center">
+            <a href="./index.php" class="navelement">Ana Sayfa</a>
+        <a href="./mysupport.php" class="navelement">Destek Taleplerim</a>
+        <a href="./addsupport.php" class="navelement btn-orange">Yeni Destek Talebi Aç</a>
+        <div class="mobilenavbar">
+            <div class="line mb-1"></div>
+            <div class="line mb-1"></div>
+            <div class="line mb-1"></div>
+        </div>
+    </div>
 </div>
--- index.php
+++ index.php
@@ -4,39 +4,36 @@ include("class.function.php");
 <html lang="tr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css" rel="stylesheet" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css" rel="stylesheet" />
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css" rel="stylesheet" />
 
-    <link rel="stylesheet" href="apps/css/style.css">
+  <link rel="stylesheet" href="apps/css/style.css">
 </head>
 
 <body>
-<div class="svg-container">
-    <svg viewbox="0 0 800 400" class="svg">
-      <path id="curve" fill="#50c6d8c2" d="M 800 300 Q 400 350 0 300 L 0 0 L 800 0 L 800 300 Z">
-      </path>
-    </svg>
+  
+  <div class="navcontainer">
+    <?php include("apps/includes/navbar.php"); ?>
   </div>
-    <div class="navcontainer">
-        <?php include("apps/includes/navbar.php"); ?>
-    </div>
-    <div class="container mt-5 mb-5">
-
-        <header>
-            <h1>Destek Sistemimiz İle</h1>
-            <h3>Sorunlarınıza Anlık Çözüm.</h3>
-        </header>
-    </div>
+   <div class='code'>
+  <header class='content'>
+    <h1 class='title'>Gappze Destek Sistemi</h1>
+    <div class='text'>En güncel sistemlerle yapılan destek sistemimiz ile anında yanıt alın.</div>
+  </header>
+ 
+  <div class='wave'></div>
+  <div class='wave wave2'></div>
+  <div class='wave wave3'></div>
 </body>
 <script src="apps/js/script.js"></script>
 
